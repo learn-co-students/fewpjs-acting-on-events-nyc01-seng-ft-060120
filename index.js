6735adeedeae449904677ffe2455291f845505d2
shiro-han <shan99business@gmail.com>
2020-07-13 22:53:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`
+    }
+}
+
+function moveDodgerDown() {
+    let bottomNumbers = dodger.style.bottom.replace("px", "");
+    let bottom = parseInt(bottomNumbers, 10);
+    
+    if (bottom > 0) {
+        dodger.style.bottom = `${bottom - 10}px`
+    }
+}
+
+function moveDodgerUp() {
+    let bottomNumbers = dodger.style.bottom.replace("px", "");
+    let bottom = parseInt(bottomNumbers, 10);
+    
+    if (bottom < 360) {
+        dodger.style.bottom = `${bottom + 10}px`
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+
+});
