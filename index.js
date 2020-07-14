6c22b1529ebfa48207486938381ba588ad70a093
alexpac27 <alexpac27@gmail.com>
2020-07-14 12:55:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+
+let dodger = document.getElementById("dodger");
+
+// MOVING LEFT
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+// MOVING RIGHT
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+   
+    if (right < 400) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+
+//EVEN LISTENER
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
+
+
